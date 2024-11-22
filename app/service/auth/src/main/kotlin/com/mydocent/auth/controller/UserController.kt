aa8551f6da83fa8cc496dc3dbe27def78923256d
kangminseok <tootkms95@toothlife.co.kr>
2024-11-22 13:19:37 +0900

    user api 수정

--- app/service/auth/src/main/kotlin/com/mydocent/auth/controller/UserController.kt
+++ app/service/auth/src/main/kotlin/com/mydocent/auth/controller/UserController.kt
@@ -1,13 +1,21 @@
 package com.mydocent.auth.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api")
 class UserController {
     @GetMapping("/auth-users")
-    fun requiredAuth() = "인증된 요청만 받을 수 있어!"
+    fun requiredAuth(): String {
+        println("auth-users")
+        return "인증된 요청만 받을 수 있어!"
+    }
 
     @GetMapping("/free-users")
-    fun freeAuth() = "인증하지 않아도 받을 수 있어!"
+    fun freeAuth(): String {
+        println("free-users 접근!")
+        return "인증하지 않아도 받을 수 있어!"
+    }
 }
