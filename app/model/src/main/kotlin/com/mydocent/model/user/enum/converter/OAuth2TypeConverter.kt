e847f8aacd345a9e407dc2c3e5c46e25bd8f740e
kangminseok <tootkms95@toothlife.co.kr>
2024-11-26 18:46:10 +0900

    feat : oauth2 enum convert 기능 추가

--- app/model/src/main/kotlin/com/mydocent/model/user/enum/converter/OAuth2TypeConverter.kt
+++ app/model/src/main/kotlin/com/mydocent/model/user/enum/converter/OAuth2TypeConverter.kt
@@ -9,6 +9,13 @@ class OAuth2TypeConverter: AttributeConverter<OAuth2Type, String> {
 
     override fun convertToDatabaseColumn(enumTypeOAuth: OAuth2Type): String = OAuth2Type.KAKAO.code.toString()
 
-    override fun convertToEntityAttribute(stringTypeOAuth: String): OAuth2Type = OAuth2Type.of(stringTypeOAuth.toInt())
+    override fun convertToEntityAttribute(stringTypeOAuth: String): OAuth2Type {
+        // TODO: db에 oauth2 타입을 숫자로 넣을지 문자로 넣을지 정하기
+        return if(stringTypeOAuth == "kakao") {
+            OAuth2Type.KAKAO
+        } else {
+            OAuth2Type.of(stringTypeOAuth.toInt())
+        }
+    }
 
 }
