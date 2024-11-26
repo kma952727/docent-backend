a392bbdfbf624ae2f9e198c69b1b705bfe779cdd
kangminseok <tootkms95@toothlife.co.kr>
2024-11-26 18:46:24 +0900

    feat : 간단한 log 예시 추가

--- app/service/user/src/main/kotlin/com/mydocent/user/controller/UserController.kt
+++ app/service/user/src/main/kotlin/com/mydocent/user/controller/UserController.kt
@@ -4,6 +4,7 @@ import com.mydocent.model.user.dto.CreateUserApiDto
 import com.mydocent.shared.api.ApiResponse
 import com.mydocent.model.user.dto.FindAllUsersApiDto
 import com.mydocent.user.service.UserService
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/users")
 class UserController(private val userService: UserService) {
+    private val log = KotlinLogging.logger { }
 
     /**
      * test용 API입니다.
@@ -22,6 +24,7 @@ class UserController(private val userService: UserService) {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     fun findAllUsers(): ApiResponse<List<FindAllUsersApiDto.Response>> {
+        log.info { "${::findAllUsers.name} ::" }
         return ApiResponse(data=userService.findAllUsers())
     }
 
@@ -31,6 +34,7 @@ class UserController(private val userService: UserService) {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun createUsers(@RequestBody createUserApiDto: CreateUserApiDto.Request): ApiResponse<CreateUserApiDto.Response> {
+        log.info { "${::createUsers.name} :: ${createUserApiDto::class.simpleName} :: $createUserApiDto" }
         return ApiResponse(data=userService.createUser(createUserApiDto))
     }
 }
