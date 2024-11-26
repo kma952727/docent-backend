a0b7d68fb611ab10369183a54b05c06a91646e4a
kangminseok <tootkms95@toothlife.co.kr>
2024-11-26 18:45:39 +0900

    feat : log 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,12 +20,13 @@ version = "0.0.1"
 
 
 allprojects {
-	repositories {
-		mavenCentral()
-	}
-
 	plugins.apply("org.jetbrains.kotlin.plugin.allopen")
 	plugins.apply("org.jetbrains.kotlin.plugin.noarg")
+	/**
+	 * implementation 함수를 사용하려면
+	 * `java` or `org.jetbrains.kotlin.jvm` plugin 필요
+	 */
+	plugins.apply("org.jetbrains.kotlin.jvm")
 
 	allOpen {
 		annotation("jakarta.persistence.Entity")
@@ -36,6 +37,14 @@ allprojects {
 		annotation("jakarta.persistence.Entity")
 		annotation("jakarta.persistence.MappedSuperclass")
 		annotation("jakarta.persistence.Embeddable")
+	}
+
+	dependencies {
+		implementation("io.github.oshai:kotlin-logging-jvm:5.1.1")
+	}
+
+	repositories {
+		mavenCentral()
 	}
 }
 
